d2757f85591f71339040a9132a21899803c47d63
amanovishnu <geekymano@gmail.com>
2020-12-22 14:36:31 +0530

    Admin Page configured

--- pages/views.py
+++ pages/views.py
@@ -10,10 +10,19 @@ def home(request):
     teamData = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+    model_search = Car.objects.values_list('model',flat=True).distinct()
+    city_search = Car.objects.values_list('city',flat=True).distinct()
+    year_search = Car.objects.values_list('year',flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style',flat=True).distinct()
+
     data = {
         'teamData': teamData,
         'featured_cars': featured_cars,
-        'all_cars': all_cars
+        'all_cars': all_cars,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style_search':body_style_search
         }
     return render(request, 'pages/home.html', context=data)
 
