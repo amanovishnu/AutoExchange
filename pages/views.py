bf5da7bd33ec5a16bed14fe1ae0c3ab8ab67e91c
amanovishnu <geekymano@gmail.com>
2022-05-14 14:24:43 +0530

    Added static files

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-# Create your views here.
 def home(request):
-    return render(request, 'pages/home.html', context={'message' : 'welcome to Django tuts'})
+    return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
