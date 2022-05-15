f8ced883610b1febf3c05302f40ea12abbb44796
amanovishnu <geekymano@gmail.com>
2022-05-15 14:12:25 +0530

    pagination & details page added

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Car
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.all().order_by('-created_date')
+    paginator = Paginator(cars,2)
+    page_number = request.GET['page']
+    page_cars = paginator.get_page(page_number)
+    data = {'cars':page_cars}
+    return render(request, 'cars/cars.html', context=data)
+
+
+def car_detail(request, id):
+    car = get_object_or_404(Car, pk=id)
+    print(type(car.features))
+    data = {'car':car}
+    return render(request, 'cars/car_detail.html', context=data)
