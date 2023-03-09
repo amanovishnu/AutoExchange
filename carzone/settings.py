55e4a14fb9706e11987e37502ce411e912b47837
amanovishnu <geekymano@gmail.com>
2023-03-09 21:49:51 +0530

    info - static files & pages app added

--- carzone/settings.py
+++ carzone/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(f'BASE_DIR -> {BASE_DIR}')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # global static path
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_local'), # local or in app static path
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
